c600c60d413c2c3ba1fa1bf18c389c56009af9d8
Lola Onanuga <slonanuga@gmail.com>
2018-09-20 17:47:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,13 @@
 def fizzbuzz(number)
   
   if number % 3 == 0
-    return "Fizz"
+      "Fizz"
+      
   elsif number % 5 == 0
-    return "Buzz"
+     "Buzz"
+     
   elsif number % 3 == 0 && number % 5 == 0
-    return "FizzBuzz"
+    "FizzBuzz"
+    
+  else nil
 end
